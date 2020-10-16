482b47285b1951e49c93dd9d9236c72bcb4293ad
Erika Leão <erika.lesantos@gmail.com>
2020-10-16 19:47:48 -0300

    Key buffer

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -2,8 +2,13 @@ import { GoogleSpreadsheet } from 'google-spreadsheet'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
-export default async (req, res) => {
+const fromBase64 = value => {
+    const buff = Buffer.from(value, 'base64');
+    return buff.toString('ascii');
+}
 
+export default async (req, res) => {
+    console.log(fromBase64(process.env.SHEET_PRIVATE_KEY))
     try {
         await doc.useServiceAccountAuth({
             client_email: process.env.SHEET_CLIENT_EMAIL,
